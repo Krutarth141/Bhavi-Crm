21662c25d602c2210bc02981c50055edf6913fa0
Krutarth141 <krutarth.t@ahduni.edu.in>
2026-08-08 11:09:24 +0530

    added pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/auth.config';
+import PublicHomeScreen from '@/components/screens/PublicHomeScreen';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -9,5 +10,5 @@ export default async function Home() {
     redirect('/dashboard');
   }
 
-  redirect('/login');
+  return <PublicHomeScreen />;
 }
