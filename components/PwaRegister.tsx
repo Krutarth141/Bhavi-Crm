f50507d6c8c2b58b411d80648f5c4c4f103c0349
Krutarth141 <krutarth.t@ahduni.edu.in>
2026-08-29 16:49:34 +0530

    stale refresh

--- components/PwaRegister.tsx
+++ components/PwaRegister.tsx
@@ -4,9 +4,16 @@ import { useEffect } from 'react';
 
 export default function PwaRegister() {
     useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js').catch(() => { });
+        if (process.env.NODE_ENV !== 'production') {
+            navigator.serviceWorker.getRegistrations().then((regs) => {
+                regs.forEach((r) => r.unregister());
+            });
+            if ('caches' in window) {
+                caches.keys().then((keys) => keys.forEach((k) => caches.delete(k)));
+            }
+            return;
         }
+        navigator.serviceWorker.register('/sw.js').catch(() => { });
     }, []);
     return null;
 }
